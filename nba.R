c586527d02c5568f9f7fababde8ecdb7f46d89f6
Ramiro Bericua <ramirobericua@gmail.com>
2025-10-17 15:07:10 -0300

    Train one model per player

--- nba.R
+++ nba.R
@@ -5,6 +5,26 @@ load_package = function(package) {
   library(package, character.only = TRUE)
 }
 
+predict_for_player = function(player, data) {
+  if (!(player %in% names(points_models))) {
+    return (NA)
+  }
+  
+  points_model = points_models[[player]]
+  assists_model = assists_models[[player]]
+  off_rebounds_model = off_rebounds_models[[player]]
+  
+  predicted_points = predict(points_model, data)
+  predicted_assists = predict(assists_model, data)
+  predicted_off_rebounds = predict(off_rebounds_model, data)
+  
+  return (list(
+    points = predicted_points,
+    assists = predicted_assists,
+    off_rebounds = predicted_off_rebounds
+  ))
+}
+
 calculate_mae = function(actual, predicted) {
   return (mean(abs(actual - predicted)))
 }
@@ -48,37 +68,82 @@ for (col_name in relevant_cols) {
       sep = "")
 }
 
-training_indices = 1:(nrow(all_data) * 0.7)
-training_data = all_data[training_indices, ]
-testing_data = all_data[-training_indices, ]
+training_percentage = 0.7
+players = unique(all_data$athlete_display_name)
+points_models = list()
+assists_models = list()
+off_rebounds_models = list()
 
-points_model = lm(points ~ home_away + season_type + opponent_defensive_rating,
-               data = training_data)
-assists_model = lm(assists ~ home_away + season_type
-                   + opponent_defensive_rating,
-               data = training_data)
-off_rebounds_model = lm(offensive_rebounds ~ home_away + season_type
-                        + opponent_defensive_rating,
-               data = training_data)
+num_weight_periods = 3
+weight_period_len = nrow(training_data) / num_periods
+weights = 
 
-actual_points = testing_data$points
-predicted_points = predict(points_model, testing_data)
-actual_assists = testing_data$assists
-predicted_assists = predict(assists_model, testing_data)
-actual_off_rebounds = testing_data$offensive_rebounds
-predicted_off_rebounds = predict(off_rebounds_model, testing_data)
+for (player in players) {
+  cat("Training model for ", player, "\n", sep = "")
+  player_data = all_data %>% filter(athlete_display_name == player)
+  training_indices = 1:(nrow(player_data) * training_percentage)
+  training_data = player_data[training_indices, ]
+  testing_data = player_data[-training_indices, ]
+  
+  if (length(unique(training_data$home_away)) < 2
+      | length(unique(training_data$season_type)) < 2
+      | !all(testing_data$season_type %in% training_data$season_type)) {
+    next
+  }
 
-points_k = calculate_k(all_data$points)
-assists_k = calculate_k(all_data$assists)
-off_rebounds_k = calculate_k(all_data$offensive_rebounds)
+  points_model = lm(points ~ home_away + season_type + opponent_defensive_rating,
+                    data = training_data)
+  assists_model = lm(assists ~ home_away + season_type
+                     + opponent_defensive_rating,
+                     data = training_data)
+  off_rebounds_model = lm(offensive_rebounds ~ home_away + season_type
+                          + opponent_defensive_rating,
+                          data = training_data)
 
-points_accuracy = calculate_accuracy(actual_points, predicted_points,
-                                     points_k)
-assists_accuracy = calculate_accuracy(actual_assists, predicted_assists,
-                                      assists_k)
-off_rebounds_accuracy = calculate_accuracy(actual_off_rebounds,
-                                           predicted_off_rebounds,
-                                           off_rebounds_k)
+  points_models[[player]] = points_model
+  assists_models[[player]] = assists_model
+  off_rebounds_models[[player]] = off_rebounds_model
+}
+
+points_accuracy = 0
+assists_accuracy = 0
+off_rebounds_accuracy = 0
+
+for (player in players) {
+  player_data = all_data %>% filter(athlete_display_name == player)
+  training_indices = 1:(nrow(player_data) * training_percentage)
+  testing_data = player_data[-training_indices, ]
+  
+  prediction = predict_for_player(player, testing_data)
+  if (!is.list(prediction)) {
+    next
+  }
+  
+  cat("Predicted stats for ", player, "\n", sep = "")
+  
+  points_k = calculate_k(all_data$points)
+  assists_k = calculate_k(all_data$assists)
+  off_rebounds_k = calculate_k(all_data$offensive_rebounds)
+  
+  actual_points = testing_data$points
+  actual_assists = testing_data$assists
+  actual_off_rebounds = testing_data$offensive_rebounds
+  
+  points_accuracy = points_accuracy + calculate_accuracy(actual_points,
+                                                         prediction$points,
+                                                         points_k)
+  assists_accuracy = assists_accuracy + calculate_accuracy(actual_assists, 
+                                                           prediction$assists,
+                                                           assists_k)
+  off_rebounds_accuracy = off_rebounds_accuracy + calculate_accuracy(actual_off_rebounds,
+                                                                     prediction$off_rebounds,
+                                                                     off_rebounds_k)
+}
+
+points_accuracy = points_accuracy / length(players)
+assists_accuracy = assists_accuracy / length(players)
+off_rebounds_accuracy = off_rebounds_accuracy / length(players)
+
 global_accuracy = (points_accuracy + assists_accuracy + off_rebounds_accuracy) / 3
 
 cat("Points accuracy: ", points_accuracy, "\n", sep = "")
