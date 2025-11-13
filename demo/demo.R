5f3cc1457b6a181dbbfaa91cc857167f9a826aeb
Ramiro Bericua <ramirobericua@gmail.com>
2025-11-13 16:07:55 -0300

    Add demo

---
+++ demo/demo.R
@@ -0,0 +1,94 @@
+#            ________
+#    o      |   __   |
+#      \_ O |  |__|  |
+#   ____/ \ |___WW___|
+#   __/   /     ||
+#               ||
+#               ||
+# _______________||________________
+
+
+
+# Carga de paquetes necesarios para el proyecto
+
+load_package <- function(package) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package)
+  }
+  library(package, character.only = TRUE)
+}
+
+load_package("dplyr")
+
+load_package("hoopR")
+
+load_package("ggplot2")
+
+# Funciones auxiliares
+
+calculate_mae <- function(actual, predicted) {
+  return(mean(abs(actual - predicted)))
+}
+
+calculate_accuracy <- function(actual, predicted, k) {
+  mae <- calculate_mae(actual, predicted)
+  return(1 / (1 + mae / k))
+}
+
+all_data_clean <- read.csv("data_clean.csv", header = TRUE)
+players <- read.csv("players.csv", header = TRUE)[[1]]
+player_models <- readRDS("player_models.rds")
+
+k_coefficients <- read.csv("k_coefficients.csv", header = TRUE)
+points_k <- k_coefficients$points
+assists_k <- k_coefficients$assists
+off_rebounds_k <- k_coefficients$off_rebounds
+
+for (player in players) {
+  testing_data <- all_data_clean %>%
+    filter(athlete_display_name == player) %>%
+    slice_head()
+  
+  points_model <- player_models[[player]]$points
+  assists_model <- player_models[[player]]$assists
+  off_rebounds_model <- player_models[[player]]$off_rebounds
+
+  prediction <- data.frame(
+    points = predict(points_model, newdata = testing_data),
+    assists = predict(assists_model, newdata = testing_data),
+    off_rebounds = predict(off_rebounds_model, newdata = testing_data)
+  )
+
+  prediction[prediction < 0] <- 0
+
+  actual_points <- testing_data$points
+  actual_assists <- testing_data$assists
+  actual_off_rebounds <- testing_data$offensive_rebounds
+
+  points_accuracy <- calculate_accuracy(
+    actual_points,
+    prediction$points,
+    points_k
+  )
+  assists_accuracy <- calculate_accuracy(
+    actual_assists,
+    prediction$assists,
+    assists_k
+  )
+  off_rebounds_accuracy <- calculate_accuracy(
+    actual_off_rebounds,
+    prediction$off_rebounds,
+    off_rebounds_k
+  )
+  
+  cat("\n------------------------------------------------------\n")
+  cat(sprintf("Player: %-20s\n", player))
+  cat("------------------------------------------------------\n")
+  cat(sprintf("Stat           | Predicted | Actual | Accuracy (%%)\n"))
+  cat(sprintf("%-14s | %-9.2f | %-6.2f | %-6.2f\n",
+              "Points", prediction$points, actual_points, points_accuracy*100))
+  cat(sprintf("%-14s | %-9.2f | %-6.2f | %-6.2f\n",
+              "Assists", prediction$assists, actual_assists, assists_accuracy*100))
+  cat(sprintf("%-14s | %-9.2f | %-6.2f | %-6.2f\n",
+              "Off. Rebounds", prediction$off_rebounds, actual_off_rebounds, off_rebounds_accuracy*100))
+}
